1bbb1bc6f749b88ac294800ad46a921a835b6715
rodrigo arcanjo <rodrigoarcanjo690@gmail.com>
2024-01-12 11:38:02 -0300

    primeiro commit

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css';
+
+function Banner() {
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner príncipal da página organo"/>
+        </header>
+        
+    )
+
+}
+export default Banner
